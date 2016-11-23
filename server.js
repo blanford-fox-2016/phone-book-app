cd67f75399588dc6208390faf5b23633b421c4be
broerjuang <juangwiantoro@gmail.com>
2016-11-23 09:59:26 +0700

    adding the actions index

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
 var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
+
+var path = require('path')
+var express = require('express')
+var app = express()
+var port = 3000
+
+var compiler = webpack(config)
+
+app.use(webpackDevMiddleware(compiler, {noInfo: true, publicPath: config.output.publicPath}))
+app.use(webpackHotMiddleware(compiler))
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.listen(port, function(error) {
+  error ? console.log(error) : console.log('server is running on port ', port)
+})
